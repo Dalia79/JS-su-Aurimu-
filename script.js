18b64502921786d1c0aabbbff03d8cf4cef01e2c
Dalia Dargiene <dargida@gmail.com>
2022-01-22 12:38:52 +0200

    ANTRA UZDUOTIS

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function sayHi() {
-    alert('labas');
+function sayHi(word) {
+    alert(word);
 }
 
-sayHi(); 
+sayHi('laba diena');
 
-/* 1UZDUOTIS
-Deklaruoti funkcija sayHi;
-Ji turi kvieti "alert" su pasisveikinimu'
-Pakviesti funkcija;
-/*
+
+// 1UZDUOTIS
+// Deklaruoti funkcija sayHi;
+// Ji turi kvieti "alert" su pasisveikinimu'
+// Pakviesti funkcija;
+
+// 2 UZDUOTIS
+// papildyti koda:
+// -priimtuparametra pavadinimu "word";
+// -pakvieskit funkcija su duotu zodziu;
+// -"alert" turi butik vieciama su parametru;
+
